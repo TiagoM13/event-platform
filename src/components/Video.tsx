e6803921ef480757454755ae318530f1299d5026
TiagoM13 <thyagomotha2000@gmail.com>
2022-06-23 21:04:28 -0300

    adding routes

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,18 @@
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import { Player, Youtube, DefaultUi } from '@vime/react'
+
+import '@vime/core/themes/default.css'
 
 export function Video() {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="SO4-izct7Mc"/>
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
