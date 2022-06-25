f83ec3dcea7bab02d5f0d632d661ef9f10a93a89
TiagoM13 <thyagomotha2000@gmail.com>
2022-06-25 14:15:30 -0300

    adding navigation in subscribe component

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ohw0oz0a4301z4541m8j8q/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
